e381aa8ce729266c2e5527b48f4965d9ce11aef3
Phuong Nguyen <duyphuong61290@gmail.com>
2021-10-22 16:56:02 +0700

    Signin/ Register/ Firebase set up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
-// layout
+import { checkUserSession } from './redux/User/user.actions';
+
+// hoc
+import WithAuth from './hoc/withAuth';
+
+// layouts
 import MainLayout from './layouts/MainLayout';
 import HomepageLayout from './layouts/HomepageLayout';
+
 // pages
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
+import Login from './pages/Login';
+import Recovery from './pages/Recovery';
+import Dashboard from './pages/Dashboard';
+import './default.scss';
 
-import "./defaults.scss";
+const App = props => {
+  const dispatch = useDispatch();
 
-function App() {
+  useEffect(() => {
+    dispatch(checkUserSession());
+
+  }, []);
+
   return (
-    <Switch>
-      <Route exact path='/' render={() => (
-        <HomepageLayout>
-          <Homepage />
-        </HomepageLayout>
-      )} />
-      <Route path='/registration' render={() => (
-        <MainLayout>
-          <Registration />
-        </MainLayout>
-      )} />
-      <Route />
-    </Switch>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={() => (
+          <HomepageLayout>
+            <Homepage />
+          </HomepageLayout>
+        )}
+        />
+        <Route path="/registration" render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>
+        )} />
+        <Route path="/login"
+          render={() => (
+            <MainLayout>
+              <Login />
+            </MainLayout>
+          )} />
+        <Route path="/recovery" render={() => (
+          <MainLayout>
+            <Recovery />
+          </MainLayout>
+        )} />
+        <Route path="/dashboard" render={() => (
+          <WithAuth>
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          </WithAuth>
+        )} />
+      </Switch>
+    </div>
   );
 }
 
